f21864b3f180df9e76a224ad0bdafdb92e19e672
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2020-03-16 08:04:04 +0600

    Added framework boilerplate, created models and implemented EF core to create DB

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RajUniEFCoreRP3._1.Data;
+using System;
+
+namespace RajUniEFCoreRP3._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNoExists(host);
+
+            host.Run();
+        }
+
+        public static void CreateDbIfNoExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RajUniEFCoreRP3.1
-A web apps project of university course enrollment for students
+A web apps project of university course enrollment for students and this web apps is developed by Razor Pages ASP.NET Core 3.1 framework.
